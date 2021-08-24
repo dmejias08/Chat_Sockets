54d9bfe252f1184d6a26ce1e9dd4aa611ca6e2a3
dmejias08 <85594834+dmejias08@users.noreply.github.com>
2021-08-24 13:38:47 -0600

    Create Interface
    add buttons, labels, and textfields

---
+++ src/com/app/App.java
@@ -0,0 +1,18 @@
+package com.app;
+
+import javax.swing.*;
+
+public class App {
+    private JTextArea response;
+    private JButton btnSend;
+    private JTextField textTax;
+    private JTextField textWeight;
+    private JTextField textPrice;
+    private JLabel labelPrice;
+    private JLabel labelWeight;
+    private JLabel labelTax;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
